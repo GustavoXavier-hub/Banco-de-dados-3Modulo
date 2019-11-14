feee51fad8760ecfb07b54a62be01edb55fdef12
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-11-14 19:03:36 -0300

    Exercicio 8-nao-finalizado

--- Aula8.sql
+++ Aula8.sql
@@ -1,54 +1,89 @@
 
-	create table produtos
-	(
-	codigo int,
-	nome varchar(45),
-	comissao float,
-	valor float
-	)
-	create table vendedores
-	(
-	codico int,
-	nome varchar(45),
-	comissao float,
-	)
-	create table cliente
-	(
-	codigo int,
-	nome varchar(40)
-	)
-	create table vendas
-	(
-	nrovenda int,
-	Data smalldatetime,
-	cliente int ,
-	valortotal float
-	)
-	create table vendasitems
-	(
-	nrovenda int,
-	nroitem int ,
-	produto int,
-	valorvenda float,
-	qtde int
-	)
-	create table commissao
-	(
-	momento smalldatetime,
-	vendedor int,
-	venda int,
-	tipocomissao varchar(10),
-	valorcomissao float,
-	statuscomissao varchar(10),
-	dtpagamento smalldatetime
-	)
+CREATE TABLE PRODUTO 
+(CODIGO INT,
+ NOME VARCHAR(40),
+ COMISSAO FLOAT,
+ VALOR FLOAT)
+ GO
+CREATE TABLE VENDAS
+(NROVENDA INT,
+ DATA SMALLDATETIME,
+ CLIENTE INT,
+ VALORTOTA FLOAT,
+ VENDEDOR INT)
+ GO
+CREATE TABLE VENDEDOR 
+(CODIGO INT, 
+NOME VARCHAR(10),
+COMISSAO FLOAT)
+GO
+CREATE TABLE CLIENTE 
+(CODIGO INT,
+NOME VARCHAR(40)
+)
+GO
+CREATE TABLE VENDASITEMS 
+(NROVENDA INT,
+ NROITEM INT, 
+ PRODUTO INT,
+ VALORVENDA FLOAT,
+ QTDE INT)
+ GO
+CREATE TABLE COMISSAO
+(
+MOMENTO SMALLDATETIME, 
+VENDEDOR INT,
+VENDA INT,
+TIPOCOMISSAO CHAR(1),
+VALORCOMISSAO FLOAT, 
+STATUSCOMISSAO CHAR(1),
+DTPAGAMENTO SMALLDATETIME)
 
 
-ALTER TABLE comissao ADD CONSTRAINT vendedor_fk PRIMARY KEY (coluna1,); 
+drop table produto
+go
+drop table cliente
+go
+drop table vendasitems
+go
+drop table comissao
+go
+drop table vendedor
+go
+drop table vendas
 
-create trigger trigger01 on produtos
-for insert,delete,update
+select * from CLIENTE
 
+		insert into PRODUTO (CODIGO,NOME,COMISSAO,VALOR)
+		values (1,'xa',1.2,4.5)
+
+				insert into PRODUTO (CODIGO,NOME,COMISSAO,VALOR)
+		values (3,'cv',5.2,9.5)
+
+		insert into  VENDEDOR(CODIGO,NOME,COMISSAO)
+		values (1,'xc',1.3)
+
+		insert into CLIENTE(CODIGO,NOME)
+		values  (1,'fv')
+
+		insert into VENDAS values (1,'2019-11-12',1,100,1)
+
+		
+create trigger CALCULACOMISSAOVENDA on VENDAS for insert
 as 
 begin
- if(exists (select codigo from inserted)) 
+	declare @nrovenda int
+	declare @valortotal float 
+	declare @codigo int
+	declare @comissao float	
+
+    select @nrovenda =NROVENDA,@valortotal=VALORTOTA,@codigo =VENDEDOR   from inserted
+
+	select @comissao =COMISSAO from VENDEDOR where CODIGO=@codigo
+
+	insert into COMISSAO (MOMENTO,VENDEDOR,VENDA,TIPOCOMISSAO,VALORCOMISSAO,STATUSCOMISSAO)
+		values (CURRENT_TIMESTAMP,@codigo,@nrovenda,'v',@valortotal*@comissao,'A')
+
+end
+	
+	select * from COMISSAO
